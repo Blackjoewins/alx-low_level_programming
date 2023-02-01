d7ba23193b2d535c0553285cc0044ab4f8fc0fb6
Blackjoewins <cletuschukwu2010@yahoo.com>
2023-02-01 15:00:05 -0800

    dodle

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@ include <stdio.h>
  * main - prints exactly "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19",
  * followed by a new line, to the standard error.
- * Return: Always 0 (Success)
+ * 7Return: Always 0 (Success)
  */
 int main(void)
 {
